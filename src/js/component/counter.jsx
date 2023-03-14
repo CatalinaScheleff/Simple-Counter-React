b155b4ecdd8aa4945f8b113067b766b48265a0e1
CatalinaScheleff <catascheleff@gmail.com>
2023-03-14 23:44:34 +0000

    all digits

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,27 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 
 function SecondsCounter (props) {
    
+    let texto = props.segundos.toString()
+    let unidades = texto [texto.length-1];
+    let decenas = texto [texto.length-2];
+    let centenas = texto [texto.length-3]
+    let mil = texto [texto.length-4]
+    let decmil = texto [texto.length-5]
+
     return <div className ="bigDiv" >
         <div className = "clock"><i className="far fa-clock"></i></div>
-        <div className = "smallDiv">0</div>
-        <div className = "smallDiv">0</div>
-        <div className = "smallDiv">0</div>
-        <div className = "smallDiv">0</div>
-        <div className = "smallDiv">{props.segundos}</div>
+        <div className = "smallDiv">{decmil==undefined? 0:decmil}</div>
+        <div className = "smallDiv">{mil==undefined? 0:mil}</div>
+        <div className = "smallDiv">{centenas==undefined? 0:centenas}</div>
+        <div className = "smallDiv">{decenas==undefined? 0:decenas}</div>
+        <div className = "smallDiv">{unidades}</div>
 
     </div>
 }
 
- let counter=0;  
- setInterval( function() {
-    ReactDOM.render(
-        <SecondsCounter segundos={counter}/>,
-        document.querySelector('#app')
-    ); 
-        counter++;
-    }, 1000);
+//  let counter=0;  
+//  setInterval( function() {
+//     ReactDOM.render(
+//         <SecondsCounter segundos={counter}/>,
+//         document.querySelector('#app')
+//     ); 
+//         counter++;
+//     }, 1000);
     
 
     export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,14 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/counter.jsx";
 
+let contador = 0
+setInterval(function(){
+    console.log(contador)
+    contador++
+    ReactDOM.render(<SecondsCounter segundos= {contador} />, document.querySelector("#app"));   
+},1000);
 //render your react application
+
 ReactDOM.render(<Home />, document.querySelector("#app"));
